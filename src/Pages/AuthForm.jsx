51d2b5f3ade3590a8da03de9a0c3cc06e73fb1c8
Rushikesh  Gaware <119421184+iamrishi007@users.noreply.github.com>
2024-10-17 11:22:37 +0530

    Update AuthForm.jsx

--- src/Pages/AuthForm.jsx
+++ src/Pages/AuthForm.jsx
@@ -83,7 +83,7 @@ function AuthModal() {
           e.preventDefault();
 
           try {
-               const response = await fetch("http://localhost:3000/user/login", {
+               const response = await fetch("https://zopposproject-backend.onrender.com/user/login", {
                     method: "POST",
                     headers: {
                          "Content-Type": "application/json",
